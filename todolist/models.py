c4c169bbfc7f7849c2b46e114fd601e6e6a57987
CodeKnight <dearjinni44@gmail.com>
2025-12-12 16:59:15 +0545

    crud operations

--- todolist/models.py
+++ todolist/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
     
     
+ 
     
